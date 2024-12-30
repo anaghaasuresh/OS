dc3c7ba7635f2c0ced2a2a534d138a51d72b6c02
anaghaasuresh <anaghakovilakam@gmail.com>
2024-12-30 12:13:44 +0530

    Update palin.sh

--- palindrome/palin.sh
+++ palindrome/palin.sh
@@ -1,1 +1,16 @@
-
+echo "enter the number"
+read n
+org=$n
+rev=0
+while [ $n -ne 0 ]
+do
+i=$(($n%10))
+rev=$((i + $rev * 10))
+n=$(($n/10))
+done
+if [ $org -eq $rev ] 
+then
+echo "$org is palindrome"
+else
+echo "$org is not palindrome"
+fi
