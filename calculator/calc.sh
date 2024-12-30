dc45976a8aed59b3b75db9534dd84c52058432a1
anaghaasuresh <anaghakovilakam@gmail.com>
2024-12-30 12:11:53 +0530

    Update calc.sh

--- calculator/calc.sh
+++ calculator/calc.sh
@@ -1,1 +1,22 @@
-
+echo "enter the numbers"
+read a b
+echo "arithmetic calculator"
+while true
+do
+echo "1.Addition "
+echo "2.subtraction"
+echo "3.Multiplication"
+echo "4.Division"
+read n
+case $n in
+  1) result=$(($a+$b))
+     echo "sum=$result";;
+  2) result=$(($a-$b))
+     echo "difference=$result";;
+  3) result=$(($a*$b))
+     echo "product=$result";;
+  4) result=$(($a/$b))
+     echo "quotient=$result";;
+  *) echo "invalid choice"
+esac
+done
